9cf1369cc8a9413d88188f749f1a397b6d617871
CodeSprint <alirazajaveed84@gmail.com>
2024-05-03 12:16:31 +0500

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Zain on 2024-05-03.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
